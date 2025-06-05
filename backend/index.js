083b7ee4707a13410b2a74caa25c4ac58f2f3865
bettinasiren <bettinasiren@gmail.com>
2025-06-05 22:01:54 +0200

    added try catch in backend and but Typescript on any

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,6 @@ const uuid_1 = require("uuid");
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const port = process.env.PORT || 3002;
 const app = (0, express_1.default)();
-console.log("hej");
 dotenv_1.default.config();
 const client = new pg_1.Client({
     connectionString: process.env.PGURI,
@@ -38,37 +37,61 @@ app.use((0, cookie_parser_1.default)());
 // middleware för autenfificering
 function authenticate(request, response, next) {
     return __awaiter(this, void 0, void 0, function* () {
-        const token = request.cookies && request.cookies.tbtimer_token;
-        console.log("token received:", token);
-        if (!token) {
-            response.status(401).send("finns ingen token");
-            return;
+        try {
+            const token = request.cookies && request.cookies.tbtimer_token;
+            if (!token) {
+                response.status(401).send("Token not exists");
+                return;
+            }
+            const validToken = yield client.query("SELECT * FROM tokens WHERE token=$1", [token]);
+            if (validToken.rows.length === 0 || validToken.rows[0].token !== token) {
+                response.status(401).send("Not a valid token");
+                return;
+            }
+            request.user = { user_id: validToken.rows[0].user_id };
+            next();
         }
-        const validToken = yield client.query("SELECT * FROM tokens WHERE token=$1", [
-            token,
-        ]);
-        console.log("valid token:", validToken);
-        if (validToken.rows.length === 0 || validToken.rows[0].token !== token) {
-            response.status(401).send("inte ett giltigt tokenvärde");
-            return;
+        catch (error) {
+            if (error instanceof Error) {
+                console.error("An error has occurred during authentication.", error.message);
+                response
+                    .status(500)
+                    .send("An error has occurred during authentication:" + error.message);
+            }
+            else {
+                console.error("An error has occurred during authentication:", error);
+                response.status(500).send("An error has occurred during authentication");
+            }
         }
-        console.log("hej", validToken.rows);
-        request.user = { user_id: validToken.rows[0].user_id };
-        console.log("user:", request.user);
-        next();
     });
 }
 //--------------------Logga in, Logga ut------------
 // hämta token skicka id
 app.get("/token/:token", (request, response) => __awaiter(void 0, void 0, void 0, function* () {
-    const token = request.params.token;
-    const { rows: userId } = yield client.query("SELECT user_id FROM tokens WHERE token=$1", [token]);
-    if (!userId) {
-        response.status(404).send("userId finns ej");
-        return;
+    try {
+        const token = request.params.token;
+        const { rows: userId } = yield client.query("SELECT user_id FROM tokens WHERE token=$1", [token]);
+        if (!userId) {
+            response.status(404).send("userId not exists ");
+            return;
+        }
+        console.log(userId[0]);
+        response.status(200).send(userId[0]);
     }
-    console.log(userId[0]);
-    response.status(200).send(userId[0]);
+    catch (error) {
+        if (error instanceof Error) {
+            console.error("An error occurred while retrieving the token:", error.message);
+            response
+                .status(500)
+                .send("An error occurred while retrieving the token:" + error.message);
+        }
+        else {
+            console.error("An error occurred while retrieving the token:", error);
+            response
+                .status(500)
+                .send("An error occurred while retrieving the token.");
+        }
+    }
 }));
 //login
 app.post("/login", (request, response) => __awaiter(void 0, void 0, void 0, function* () {
@@ -76,7 +99,7 @@ app.post("/login", (request, response) => __awaiter(void 0, void 0, void 0, func
     try {
         const existingUser = yield client.query("SELECT * FROM users WHERE email=$1 AND password=$2", [email, password]);
         if (existingUser.rows.length === 0) {
-            response.status(401).send("Epost eller lösenord finns ej");
+            response.status(401).send("Email and passoword does not exists");
         }
         const user_id = existingUser.rows[0].id;
         const token = (0, uuid_1.v4)();
@@ -88,19 +111,42 @@ app.post("/login", (request, response) => __awaiter(void 0, void 0, void 0, func
         response.status(201).send(request.cookies.token);
     }
     catch (error) {
-        response.status(500).send("ett fel har inträffat vid inloggningen" + error);
+        if (error instanceof Error) {
+            console.error("An error has occurred during login:", error.message);
+            response
+                .status(500)
+                .send("An error has occurred during login:" + error.message);
+        }
+        else {
+            console.error("An error has occurred during login:" + error);
+            response.status(500).send("An error has occurred during login.");
+        }
     }
 }));
 // logout
 app.post("/logout", authenticate, (request, response) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
-    const user_id = (_a = request.user) === null || _a === void 0 ? void 0 : _a.user_id;
-    if (!user_id) {
-        response.status(401).send("du är inte inloggad");
+    try {
+        const user_id = (_a = request.user) === null || _a === void 0 ? void 0 : _a.user_id;
+        if (!user_id) {
+            response.status(401).send("You are not logged in");
+        }
+        yield client.query("DELETE FROM tokens WHERE user_id=$1", [user_id]);
+        response.clearCookie("tbtimer_token");
+        response.status(200).send();
     }
-    yield client.query("DELETE FROM tokens WHERE user_id=$1", [user_id]);
-    response.clearCookie("tbtimer_token");
-    response.status(200).send();
+    catch (error) {
+        if (error instanceof Error) {
+            console.error("An error has occurred during logout:", error.message);
+            response
+                .status(500)
+                .send("An error has occurred during logout:" + error.message);
+        }
+        else {
+            console.error("An error has occurred during logout:", error);
+            response.status(500).send("An error has occurred during logout.");
+        }
+    }
 }));
 //--------------------Användare---------------------
 // skapa användare
@@ -116,47 +162,125 @@ app.post("/user", (request, response) => __awaiter(void 0, void 0, void 0, funct
         response.status(201).send(user[0]);
     }
     catch (error) {
+        if (error instanceof Error) {
+            console.error("Error creating user", error);
+            response
+                .status(500)
+                .send("Error while creating a new user:" + error.message);
+        }
         console.error("Error creating user", error);
-        response.status(500).send({ error: "Error while creating a new user" });
+        response.status(500).send("Error while creating a new user");
     }
 }));
 // hämta användare (baserat på id)
 app.get("/user/:id", (request, response) => __awaiter(void 0, void 0, void 0, function* () {
-    const id = request.params.id;
-    const { rows: user } = yield client.query("SELECT * FROM users WHERE id=$1", [
-        id,
-    ]);
-    if (user.length === 0)
-        response.status(404).send();
-    response.send(user[0]);
+    try {
+        const id = request.params.id;
+        const { rows: user } = yield client.query("SELECT * FROM users WHERE id=$1", [id]);
+        if (user.length === 0)
+            response.status(404).send("User not found");
+        response.send(user[0]);
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            console.error("An error occurred while retrieving the user:", error.message);
+            response
+                .status(500)
+                .send("An error occurred while retrieving the user:" + error.message);
+        }
+        else {
+            console.error("An error occurred while retrieving the user:", error);
+            response.status(500).send("An error occurred while retrieving the userS");
+        }
+    }
 }));
 // hämta alla avatarer
 app.get("/avatars", (_request, response) => __awaiter(void 0, void 0, void 0, function* () {
-    const { rows: avatar } = yield client.query("SELECT * FROM avatars");
-    response.send(avatar);
+    try {
+        const { rows: avatar } = yield client.query("SELECT * FROM avatars");
+        response.send(avatar);
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            console.error("An error occurred while retrieving avatars:", error.message);
+            response
+                .status(500)
+                .send("An error occurred while retrieving avatars: " + error.message);
+        }
+        else {
+            console.error("An unknown error occurred:", error);
+            response.status(500).send("An unknown error occurred");
+        }
+    }
 }));
 //hämta specifik avatar kopplat till användaren
 app.get("/avatars/:id", (request, response) => __awaiter(void 0, void 0, void 0, function* () {
-    const avatarId = request.params.id;
-    const { rows: avatar } = yield client.query("SELECT avatar FROM avatars WHERE id=$1", [avatarId]);
-    response.send(avatar[0]);
+    try {
+        const avatarId = request.params.id;
+        const { rows: avatar } = yield client.query("SELECT avatar FROM avatars WHERE id=$1", [avatarId]);
+        response.send(avatar[0]);
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            console.error("An error occurred while retrieving user avatar:", error.message);
+            response
+                .status(500)
+                .send("An error occurred while retrieving user avatar: " + error.message);
+        }
+        else {
+            console.error("An unknown error occurred:", error);
+            response.status(500).send("An unknown error occurred");
+        }
+    }
 }));
 //-----------------Borsta tänderna-------------------
 // lägg till ett värde för varje gång man har borstat tänderna
 app.post("/brushing/:id", (request, response) => __awaiter(void 0, void 0, void 0, function* () {
-    const userId = request.params.id;
-    const brushingSession = yield client.query("INSERT INTO brushing_tracker (user_id) VALUES ($1)", [userId]);
-    if (brushingSession.rowCount === 0) {
-        response.status(500).send("Kunde inte lägga in i databasen");
+    try {
+        const userId = request.params.id;
+        const brushingSession = yield client.query("INSERT INTO brushing_tracker (user_id) VALUES ($1)", [userId]);
+        if (brushingSession.rowCount === 0) {
+            response.status(500).send("Could not insert into database");
+        }
+        const { rows: brushingSessionUser } = yield client.query("SELECT * FROM brushing_tracker WHERE user_id=$1", [
+            userId,
+        ]);
+        response.send(brushingSessionUser);
     }
-    const { rows: brushingSessionUser } = yield client.query("SELECT * FROM brushing_tracker WHERE user_id=$1", [userId]);
-    response.send(brushingSessionUser);
+    catch (error) {
+        if (error instanceof Error) {
+            console.error("An error occurred while processing the brushing session:", error.message);
+            response
+                .status(500)
+                .send("An error occurred while processing the brushing session: " +
+                error.message);
+        }
+        else {
+            console.error("An unknown error occurred:", error);
+            response.status(500).send("An unknown error occurred");
+        }
+    }
 }));
 //hämta alla tandborsts-sessioner per användare
 app.get("/brushing-sessions/:id", (request, response) => __awaiter(void 0, void 0, void 0, function* () {
-    const userId = request.params.id;
-    const { rows: brushingSession } = yield client.query("SELECT * FROM brushing_tracker WHERE user_id=$1", [userId]);
-    response.send(brushingSession);
+    try {
+        const userId = request.params.id;
+        const { rows: brushingSession } = yield client.query("SELECT * FROM brushing_tracker WHERE user_id=$1", [userId]);
+        response.send(brushingSession);
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            console.error("An error occurred while retrieving the brushing session:", error.message);
+            response
+                .status(500)
+                .send("An error occurred while retrieving the brushing session: " +
+                error.message);
+        }
+        else {
+            console.error("An unknown error occurred:", error);
+            response.status(500).send("An unknown error occurred");
+        }
+    }
 }));
 const StartServer = () => __awaiter(void 0, void 0, void 0, function* () {
     yield client.connect();
--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,6 @@
 import cors from "cors";
 import express, { Request, Response, NextFunction } from "express";
-import path from "path"
+import path from "path";
 import dotenv from "dotenv";
 import { Client } from "pg";
 import { v4 as uuidv4 } from "uuid";
@@ -9,8 +9,6 @@ import cookieParser from "cookie-parser";
 const port = process.env.PORT || 3002;
 const app = express();
 
-console.log("hej")
-
 dotenv.config();
 
 const client = new Client({
@@ -18,9 +16,13 @@ const client = new Client({
 });
 
 interface MyRequest extends Request {
+  cookies: Cookies;
   user?: {
     user_id: number;
   };
+}
+interface Cookies {
+  tbtimer_token?: string;
 }
 
 interface UserType {
@@ -30,27 +32,27 @@ interface UserType {
   selectedAvatar: number;
 }
 
+interface UserIdResult {
+  user_id: string;
+}
+
+interface User {
+  id: string;
+  email: string;
+  password: string;
+  avatar_id: number;
+}
+
 interface AvatarType {
   avatar: string;
   id: number;
-}
-
-interface MedalType {
-  medal_name: string;
-  medal_image: string;
-  criteria: string;
-  user_id: number;
 }
 
 interface BrushingSessionType {
   user_id: number;
 }
 
-interface TokenType {
-  token: string;
-}
-
-app.use(express.static(path.join(path.resolve(), "dist")))
+app.use(express.static(path.join(path.resolve(), "dist")));
 
 //har kvar denna för att kunna publicera på github-pages och egen server i framtiden
 app.use(
@@ -69,63 +71,93 @@ async function authenticate(
   response: Response,
   next: NextFunction
 ) {
-  const token: string = request.cookies && request.cookies.tbtimer_token;
-  console.log("token received:", token);
+  try {
+    const token: string | undefined =
+      request.cookies && request.cookies.tbtimer_token;
 
-  if (!token) {
-    response.status(401).send("finns ingen token");
-    return;
+    if (!token) {
+      response.status(401).send("Token not exists");
+      return;
+    }
+
+    const validToken = await client.query(
+      "SELECT * FROM tokens WHERE token=$1",
+      [token]
+    );
+
+    if (validToken.rows.length === 0 || validToken.rows[0].token !== token) {
+      response.status(401).send("Not a valid token");
+      return;
+    }
+
+    request.user = { user_id: validToken.rows[0].user_id };
+
+    next();
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(
+        "An error has occurred during authentication.",
+        error.message
+      );
+      response
+        .status(500)
+        .send("An error has occurred during authentication:" + error.message);
+    } else {
+      console.error("An error has occurred during authentication:", error);
+      response.status(500).send("An error has occurred during authentication");
+    }
   }
-
-  const validToken = await client.query("SELECT * FROM tokens WHERE token=$1", [
-    token,
-  ]);
-
-  console.log("valid token:", validToken);
-  if (validToken.rows.length === 0 || validToken.rows[0].token !== token) {
-    response.status(401).send("inte ett giltigt tokenvärde");
-    return;
-  }
-
-  console.log("hej", validToken.rows);
-  request.user = { user_id: validToken.rows[0].user_id };
-  console.log("user:", request.user);
-  next();
 }
-
 
 //--------------------Logga in, Logga ut------------
 
 // hämta token skicka id
 app.get("/token/:token", async (request, response) => {
-  const token = request.params.token;
+  try {
+    const token = request.params.token;
 
-  const { rows: userId } = await client.query(
-    "SELECT user_id FROM tokens WHERE token=$1",
-    [token]
-  );
+    const { rows: userId }: { rows: UserIdResult[] } = await client.query(
+      "SELECT user_id FROM tokens WHERE token=$1",
+      [token]
+    );
 
-  if (!userId) {
-    response.status(404).send("userId finns ej");
-    return
+    if (!userId) {
+      response.status(404).send("userId not exists ");
+      return;
+    }
+
+    console.log(userId[0]);
+    response.status(200).send(userId[0]);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(
+        "An error occurred while retrieving the token:",
+        error.message
+      );
+      response
+        .status(500)
+        .send("An error occurred while retrieving the token:" + error.message);
+    } else {
+      console.error("An error occurred while retrieving the token:", error);
+      response
+        .status(500)
+        .send("An error occurred while retrieving the token.");
+    }
   }
-
-  console.log(userId[0])
-  response.status(200).send(userId[0]);
 });
 
 //login
-app.post("/login", async (request, response) => {
-  const { email, password }: UserType = request.body;
+app.post("/login", async (request: Request<{}, {}, UserType>, response) => {
+  const { email, password } = request.body;
 
   try {
-    const existingUser = await client.query(
+    const existingUser = await client.query<User>(
       "SELECT * FROM users WHERE email=$1 AND password=$2",
       [email, password]
     );
 
     if (existingUser.rows.length === 0) {
-      response.status(401).send("Epost eller lösenord finns ej");
+      response.status(401).send("Email and passoword does not exists");
     }
     const user_id = existingUser.rows[0].id;
     const token = uuidv4();
@@ -135,10 +167,21 @@ app.post("/login", async (request, response) => {
       token,
     ]);
 
-    response.setHeader("Set-Cookie", `tbtimer_token=${token}; Path=/; Partitioned`);
+    response.setHeader(
+      "Set-Cookie",
+      `tbtimer_token=${token}; Path=/; Partitioned`
+    );
     response.status(201).send(request.cookies.token);
   } catch (error) {
-    response.status(500).send("ett fel har inträffat vid inloggningen" + error);
+    if (error instanceof Error) {
+      console.error("An error has occurred during login:", error.message);
+      response
+        .status(500)
+        .send("An error has occurred during login:" + error.message);
+    } else {
+      console.error("An error has occurred during login:" + error);
+      response.status(500).send("An error has occurred during login.");
+    }
   }
 });
 
@@ -147,14 +190,26 @@ app.post(
   "/logout",
   authenticate,
   async (request: MyRequest, response: Response) => {
-    const user_id = request.user?.user_id;
-    if (!user_id) {
-      response.status(401).send("du är inte inloggad");
-    }
-    await client.query("DELETE FROM tokens WHERE user_id=$1", [user_id]);
-    response.clearCookie("tbtimer_token");
+    try {
+      const user_id = request.user?.user_id;
+      if (!user_id) {
+        response.status(401).send("You are not logged in");
+      }
+      await client.query("DELETE FROM tokens WHERE user_id=$1", [user_id]);
+      response.clearCookie("tbtimer_token");
 
-    response.status(200).send();
+      response.status(200).send();
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("An error has occurred during logout:", error.message);
+        response
+          .status(500)
+          .send("An error has occurred during logout:" + error.message);
+      } else {
+        console.error("An error has occurred during logout:", error);
+        response.status(500).send("An error has occurred during logout.");
+      }
+    }
   }
 );
 
@@ -164,7 +219,7 @@ app.post("/user", async (request, response) => {
   const { username, password, email, selectedAvatar }: UserType = request.body;
   console.log(request.body);
   try {
-    const { rows: avatars } = await client.query(
+    const { rows: avatars }: { rows: AvatarType[] } = await client.query(
       "SELECT * FROM avatars WHERE id = $1",
       [selectedAvatar]
     );
@@ -173,77 +228,168 @@ app.post("/user", async (request, response) => {
       response.status(400).send("No avatar exists");
     }
 
-    const { rows: user } = await client.query(
+    const { rows: user }: { rows: User[] } = await client.query(
       "INSERT INTO users (username, password, email, avatar_id) VALUES ($1, $2, $3, $4) RETURNING *",
       [username, password, email, selectedAvatar]
     );
 
     response.status(201).send(user[0]);
   } catch (error) {
+    if (error instanceof Error) {
+      console.error("Error creating user", error);
+      response
+        .status(500)
+        .send("Error while creating a new user:" + error.message);
+    }
     console.error("Error creating user", error);
-    response.status(500).send({ error: "Error while creating a new user" });
+    response.status(500).send("Error while creating a new user");
   }
 });
 
 // hämta användare (baserat på id)
 app.get("/user/:id", async (request, response) => {
-  const id = request.params.id;
-  const { rows: user } = await client.query("SELECT * FROM users WHERE id=$1", [
-    id,
-  ]);
+  try {
+    const id = request.params.id;
+    const { rows: user }: { rows: User[] } = await client.query(
+      "SELECT * FROM users WHERE id=$1",
+      [id]
+    );
 
-  if (user.length === 0) response.status(404).send();
+    if (user.length === 0) response.status(404).send("User not found");
 
-  response.send(user[0]);
+    response.send(user[0]);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(
+        "An error occurred while retrieving the user:",
+        error.message
+      );
+      response
+        .status(500)
+        .send("An error occurred while retrieving the user:" + error.message);
+    } else {
+      console.error("An error occurred while retrieving the user:", error);
+      response.status(500).send("An error occurred while retrieving the userS");
+    }
+  }
 });
 
 // hämta alla avatarer
 app.get("/avatars", async (_request, response) => {
-  const { rows: avatar } = await client.query("SELECT * FROM avatars");
-  response.send(avatar);
+  try {
+    const { rows: avatar }: { rows: AvatarType[] } = await client.query(
+      "SELECT * FROM avatars"
+    );
+    response.send(avatar);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(
+        "An error occurred while retrieving avatars:",
+        error.message
+      );
+      response
+        .status(500)
+        .send("An error occurred while retrieving avatars: " + error.message);
+    } else {
+      console.error("An unknown error occurred:", error);
+      response.status(500).send("An unknown error occurred");
+    }
+  }
 });
 
 //hämta specifik avatar kopplat till användaren
 app.get("/avatars/:id", async (request, response) => {
-  const avatarId = request.params.id;
-  const { rows: avatar } = await client.query(
-    "SELECT avatar FROM avatars WHERE id=$1",
-    [avatarId]
-  );
-  response.send(avatar[0]);
+  try {
+    const avatarId = request.params.id;
+    const { rows: avatar }: { rows: AvatarType[] } = await client.query(
+      "SELECT avatar FROM avatars WHERE id=$1",
+      [avatarId]
+    );
+    response.send(avatar[0]);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(
+        "An error occurred while retrieving user avatar:",
+        error.message
+      );
+      response
+        .status(500)
+        .send(
+          "An error occurred while retrieving user avatar: " + error.message
+        );
+    } else {
+      console.error("An unknown error occurred:", error);
+      response.status(500).send("An unknown error occurred");
+    }
+  }
 });
-
 
 //-----------------Borsta tänderna-------------------
 // lägg till ett värde för varje gång man har borstat tänderna
 app.post("/brushing/:id", async (request, response) => {
-  const userId = request.params.id;
+  try {
+    const userId = request.params.id;
 
-  const brushingSession = await client.query(
-    "INSERT INTO brushing_tracker (user_id) VALUES ($1)",
-    [userId]
-  );
+    const brushingSession = await client.query<BrushingSessionType>(
+      "INSERT INTO brushing_tracker (user_id) VALUES ($1)",
+      [userId]
+    );
 
-  if (brushingSession.rowCount === 0) {
-    response.status(500).send("Kunde inte lägga in i databasen");
+    if (brushingSession.rowCount === 0) {
+      response.status(500).send("Could not insert into database");
+    }
+
+    const { rows: brushingSessionUser }: { rows: BrushingSessionType[] } =
+      await client.query("SELECT * FROM brushing_tracker WHERE user_id=$1", [
+        userId,
+      ]);
+
+    response.send(brushingSessionUser);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(
+        "An error occurred while processing the brushing session:",
+        error.message
+      );
+      response
+        .status(500)
+        .send(
+          "An error occurred while processing the brushing session: " +
+            error.message
+        );
+    } else {
+      console.error("An unknown error occurred:", error);
+      response.status(500).send("An unknown error occurred");
+    }
   }
-
-  const { rows: brushingSessionUser } = await client.query(
-    "SELECT * FROM brushing_tracker WHERE user_id=$1",
-    [userId]
-  );
-
-  response.send(brushingSessionUser);
 });
 
 //hämta alla tandborsts-sessioner per användare
 app.get("/brushing-sessions/:id", async (request, response) => {
-  const userId = request.params.id;
-  const { rows: brushingSession } = await client.query(
-    "SELECT * FROM brushing_tracker WHERE user_id=$1",
-    [userId]
-  );
-  response.send(brushingSession);
+  try {
+    const userId = request.params.id;
+    const { rows: brushingSession } : {rows: BrushingSessionType[]} = await client.query(
+      "SELECT * FROM brushing_tracker WHERE user_id=$1",
+      [userId]
+    );
+    response.send(brushingSession);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(
+        "An error occurred while retrieving the brushing session:",
+        error.message
+      );
+      response
+        .status(500)
+        .send(
+          "An error occurred while retrieving the brushing session: " +
+            error.message
+        );
+    } else {
+      console.error("An unknown error occurred:", error);
+      response.status(500).send("An unknown error occurred");
+    }
+  }
 });
 
 const StartServer = async () => {
